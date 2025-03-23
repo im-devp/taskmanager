cbcc523f9d3370f65ea8eaf7dca1ece01f6cef97
DevP <136971398+im-devp@users.noreply.github.com>
2025-03-23 13:33:30 +0545

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,15 +1,6 @@
 """Start the bot."""
 
-# -------------------------------------------------------------------------------------------------
-# Ivone
-#
-# Ivone is a task management bot for teams using Discord.
-# Works using discord.py and discord-py-slash-command.
-#
-# 2019 - 2021, Bernardo B. Zomer
-#
-# Source code: https://github.com/Bernardozomer/ivone-bot/
-# -------------------------------------------------------------------------------------------------
+
 
 from core import bot, hidden
 
