d7a2bcc7784e26709c5f67df20028aed8add0951
DevP <136971398+im-devp@users.noreply.github.com>
2025-03-23 13:49:40 +0545

    Create hidden.py

---
+++ src/hidden.py
@@ -0,0 +1,3 @@
+TOKEN = 'MTM1MzI3MjA4OTk2NDE4NzY1OQ.GdBXTj.ASDyX1VXcPL5ioV3TBrjcZqx3Tat9DYGBd_ZqY'
+FEEDBACK_CHANNEL_ID = 1238832819648860191  # Change for the ID of the channel you wish to use for user feedback.
+DEVELOPER_IDS = [1025056188125151336]  # Fill with the ID of users you want to have developer-level access to the bot.
